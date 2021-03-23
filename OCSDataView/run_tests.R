6dd6cfd11a19ce6aa01d8a1f0b77e504d21aac87
Kelsey Duffy <kduffy@osisoft.com>
2021-03-23 15:17:19 -0400

    Create run_tests.R

---
+++ OCSDataView/run_tests.R
@@ -0,0 +1,6 @@
+if (!require("pacman")) install.packages("pacman", repos = "https://cran.r-project.org", quiet = TRUE)
+require("pacman", quietly = TRUE)
+p_load("testthat", "xml2")
+
+options(testthat.output_file = ".\\output.xml")
+test_results <- testthat::test_local(reporter="junit")
