85fbf62a9099dc9061dfbae2b71c10d89df4b4b4
Kelsey Duffy <kduffy@osisoft.com>
2021-03-24 12:06:29 -0400

    can it find the library here

--- OCSDataView/tests/testthat/test-authentication.R
+++ OCSDataView/tests/testthat/test-authentication.R
@@ -1,11 +1,12 @@
 test_that("authentication works", {
+  library(OCSDataView)
   # get config from yml
   #test_config <- config::get(file = "OCSDataView\\conf\\test_config.yml")
   test_config <- config::get()
   Sys.setenv(R_CONFIG_ACTIVE = "default")
 
   # get auth response
-  access_token <- OCSDataView::get_auth(test_config$client_id, test_config$client_secret)
+  access_token <- get_auth(test_config$client_id, test_config$client_secret)
 
   # confirm receipt of a string longer than 100 characters (presumed to be an access token)
   expect_gt(nchar(access_token), 100)
--- OCSDataView/tests/testthat/test-data_view_interpolated.R
+++ OCSDataView/tests/testthat/test-data_view_interpolated.R
@@ -1,18 +1,18 @@
 test_that("data view interpolated data retrieval works", {
-
+  library(OCSDataView)
   # get config from yml
   #test_config <- config::get(file = "OCSDataView\\conf\\test_config.yml")
   test_config <- config::get()
   Sys.setenv(R_CONFIG_ACTIVE = "default")
 
   # get access token
-  access_token <- OCSDataView::get_auth(test_config$client_id, test_config$client_secret)
+  access_token <- get_auth(test_config$client_id, test_config$client_secret)
 
   # data view configuration
   data_access_endpoint <- paste(test_config$resource, "/api/", test_config$api_version, sep = "")
 
   # execute the request
-  my_data_frame <- OCSDataView::get_data_view_interpolated(data_access_endpoint, test_config$tenant, test_config$namespace, test_config$test_only_dataview_id, test_config$test_only_start_index, test_config$test_only_end_index, test_config$test_only_interval, access_token)
+  my_data_frame <- get_data_view_interpolated(data_access_endpoint, test_config$tenant, test_config$namespace, test_config$test_only_dataview_id, test_config$test_only_start_index, test_config$test_only_end_index, test_config$test_only_interval, access_token)
 
   # confirm the data frame has more than a header row. This presumes that the data frame contains at least two rows of data
   expect_gt(nrow(my_data_frame), 2)
